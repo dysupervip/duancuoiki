663eb15bf438e561f11b4d0c319813d0fe4e6df6
nguyenthiicuc0-ship-it <nguyenthiicuc0@gmail.com>
2026-04-24 18:18:07 +0700

    Explosion
    dddd

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float damage = 25f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+
+            if (clipInfo.Length > 0)
+            {
+                float duration = clipInfo[0].clip.length;
+
+                Destroy(gameObject, duration);
+            }
+        }
+        else
+        {
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+            }
+        }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+
+    public void DestroyExplosion()
+    {
+        Destroy(gameObject);
+    }
+}
