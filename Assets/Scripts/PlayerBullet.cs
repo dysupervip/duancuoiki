adc1ab3fb249d123f2030aef8ed0e6a877850485
dysupervip <ngdy1126@gmail.com>
2026-04-25 14:37:29 +0700

    Hiệu ứng bắn
    Bắn enemy phun máu

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -5,6 +5,7 @@ public class PlayerBullet : MonoBehaviour
     [SerializeField] private float moveSpeed = 25f;
     [SerializeField] private float timeDestroy = 0.5f;
     [SerializeField] private float damage = 10f;
+    [SerializeField] GameObject bloodPrefabs;
     void Start()
     {
         Destroy(gameObject, timeDestroy);
@@ -27,6 +28,8 @@ public class PlayerBullet : MonoBehaviour
             if(enemy !=null)
             {
                 enemy.TakeDamage(damage);
+                GameObject blood = Instantiate(bloodPrefabs,transform.position, Quaternion.identity);
+                Destroy(blood,1f);
             }
             Destroy(gameObject);
         }
